01841729f3e01927c466d1febe040c3311cd9725
u6886181-sudo <u6886181@gmail.com>
2026-07-22 10:18:53 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,9 +23,9 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "DARK-HORIZON ON TOP", // required by Discord API, not shown in the client
         state: "stalking",     // this is what people actually see
-        type: 4,               // Custom
+        type: 5,               // Custom
       },
     ],
   },
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#28282B",
+      secondary: "#6577B3",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
